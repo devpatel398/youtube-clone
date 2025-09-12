25db8a88eb4cdb9fa08f4dc0c750ff6c50030776
devpatel398 <patelvasad398@gmail.com>
2025-09-12 17:19:45 -0500

    finished Clerk authentication

--- src/modules/home/ui/components/home-navbar/home-sidebar/personal-section.tsx
+++ src/modules/home/ui/components/home-navbar/home-sidebar/personal-section.tsx
@@ -2,6 +2,7 @@
 
 import { SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
 import { HistoryIcon, ListVideoIcon, ThumbsUpIcon } from "lucide-react";
+import { useAuth, useClerk } from "@clerk/nextjs";
 import Link from "next/link";
 
 const items = [
@@ -26,6 +27,9 @@ const items = [
 ];
 
 export const PersonalSection = () => {
+    const clerk = useClerk();
+    const { isSignedIn } = useAuth();
+
     return (
         <SidebarGroup>
             <SidebarGroupLabel>You</SidebarGroupLabel>
@@ -37,7 +41,12 @@ export const PersonalSection = () => {
                                 tooltip={item.title}
                                 asChild
                                 isActive={false}    //TODO: chagne to look at current pathname
-                                onClick={() => { }}      //TODO: Do something on click
+                                onClick={(e) => {
+                                    if (!isSignedIn && item.auth) {
+                                        e.preventDefault();
+                                        return clerk.openSignIn();
+                                    }
+                                }}
                             >
                                 <Link href={item.url}>
                                     <item.icon></item.icon>
