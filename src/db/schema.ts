f67c29cea5f3f3121d7b05449657ec9c47e02466
devpatel398 <patelvasad398@gmail.com>
2025-09-14 15:18:29 -0500

    Database setup: schema table

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, uuid, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkID: text("clerk_id").unique().notNull(),
+    name: text("name").notNull(),
+    // TODO: add banner fields
+    imageURL: text("image_url").notNull(),
+    createAT: timestamp("created_at").defaultNow().notNull(),
+    updatedAT: timestamp("updated_at").defaultNow().notNull(),
+}, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkID)]);
