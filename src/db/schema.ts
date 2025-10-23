d110ff84e123847c0658f9044c304590b7564ca2
devpatel398 <patelvasad398@gmail.com>
2025-10-23 16:12:30 -0500

    chapter 5: Webhook Sync done

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,10 +2,10 @@ import { pgTable, uuid, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
-    clerkID: text("clerk_id").unique().notNull(),
+    clerkId: text("clerk_id").unique().notNull(),
     name: text("name").notNull(),
     // TODO: add banner fields
     imageURL: text("image_url").notNull(),
     createAT: timestamp("created_at").defaultNow().notNull(),
     updatedAT: timestamp("updated_at").defaultNow().notNull(),
-}, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkID)]);
+}, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
