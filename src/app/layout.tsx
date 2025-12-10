24466dbfcdcece5694f1eee4be028d4099fba2ff
devpatel398 <patelvasad398@gmail.com>
2025-12-10 00:15:36 -0600

    Chapter 10: Studio videos completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { TRPCProvider } from "@/trpc/client";
 
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <TRPCProvider>
+            <Toaster />
             {children}
           </TRPCProvider>
         </body>
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, uuid, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -10,6 +11,10 @@ export const users = pgTable("users", {
     updatedAT: timestamp("updated_at").defaultNow().notNull(),
 }, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
 
+export const userRelations = relations(users, ({ many }) => ({
+    videos: many(videos),
+}));
+
 export const categories = pgTable("categories", {
     id: uuid("id").primaryKey().defaultRandom(),
     name: text("name").notNull().unique(),
@@ -17,3 +22,32 @@ export const categories = pgTable("categories", {
     createAT: timestamp("created_at").defaultNow().notNull(),
     updatedAT: timestamp("updated_at").defaultNow().notNull(),
 }, (t) => [uniqueIndex("name_idx").on(t.name)]);
+
+export const categoryRelations = relations(users, ({ many }) => ({
+    videos: many(videos),
+}));
+
+export const videos = pgTable("videos", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    title: text("title").notNull(),
+    description: text("description"),
+    userId: uuid("user_id").references(() => users.id, {
+        onDelete: "cascade",
+    }).notNull(),
+    categoryId: uuid("category_id").references(() => categories.id, {
+        onDelete: "set null",
+    }),
+    createAT: timestamp("created_at").defaultNow().notNull(),
+    updatedAT: timestamp("updated_at").defaultNow().notNull(),
+}); 
+
+export const videoRelations = relations(videos, ({one, many}) => ({
+    user: one(users, {
+        fields: [videos.userId],
+        references: [users.id],
+    }),
+    category: one(categories, {
+        fields: [videos.categoryId],
+        references: [categories.id],
+    }),
+}));
