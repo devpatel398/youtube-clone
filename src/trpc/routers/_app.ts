928832ab4a501309555add2fe8f0ad4b6dd4b436
devpatel398 <patelvasad398@gmail.com>
2025-12-22 22:39:27 -0600

    Chapter 21: Subscriptions completed (last chapter was 20, typo 19)

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,6 +4,8 @@ import { studioRouter } from '@/modules/studio/server/procedures';
 import { videosRouter } from '@/modules/videos/server/procedures';
 import { videoViewsRouter } from '@/modules/video-views/server/procedures';
 import { videoReactionsRouter } from '@/modules/video-reactions/server/procedures';
+import { subscriptionsRouter } from '@/modules/subscriptions/server/procedures';
+
 
 
 export const appRouter = createTRPCRouter({
@@ -12,6 +14,7 @@ export const appRouter = createTRPCRouter({
   videos: videosRouter,
   videoViews: videoViewsRouter,
   videoReactions: videoReactionsRouter,
+  subscriptions: subscriptionsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
